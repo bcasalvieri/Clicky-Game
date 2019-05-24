1f61bdd0b7e28d6a5347ac5bf3d8b2a5b1bb2532
Brad Cas <bmcasalvieri@gmail.com>
2019-05-24 13:47:19 -0400

    made header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const style = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0 50px'
+  }
+}
+
+const Header = props => {
+  return (
+    <div style={style.header}>
+      <h1>Clicky Game</h1>
+      <h2>{props.msg}</h2>
+      <div style={{display: 'flex'}}>
+        <h2 style={{margin: '10px'}}>Score: {props.score}</h2>
+        <h2 style={{margin: '10px'}}>Top Score: {props.topScore}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
